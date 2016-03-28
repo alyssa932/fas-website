2dbf2cd5914ebbfe5cd277ce1edd671a655c6522
Alyssa Galganov <alyssa.galganov@utdallas.edu>
2016-03-28 10:43:50 -0500

    Initial commit
    This is the first commit with all the files and stuff.

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+/** Gulp file for tasks */
+
+var paths = {
+	templates: './templates',
+	sass: 'css/source',
+	css: 'css',
+	js: 'js',
+	bootstrapjs: 'vendor/bower_components/bootstrap-sass/assets/javascripts',
+	jquery: 'vendor/bower_components/jquery/dist'
+};
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var fileinclude = require('gulp-file-include');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var notify = require('gulp-notify');
+
+gulp.task('html', function() {
+	return gulp.src(paths.templates + '/*.tpl.html')
+		.pipe(fileinclude())
+		.pipe(rename({
+			extname: ""
+		}))
+		.pipe(rename({
+			extname: ".html"
+		}))
+		.pipe(gulp.dest('./'))
+		.pipe(notify("HTML compiled."));
+});
+
+gulp.task('sass', function() {
+	return sass(paths.sass + '/style.scss', {
+			style: 'compressed',
+			sourcemap: true,
+			cacheLocation: '/tmp/sass-cache'
+		})
+		.on('error', function (err) {
+			console.error('Error!', err.message);
+		})
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.css))
+		.pipe(notify("Sass compiled."));
+});
+
+gulp.task('copy_bootstrapjs', function() {
+	return gulp.src(paths.bootstrapjs + '/bootstrap.min.js')
+		.pipe(gulp.dest(paths.js));
+});
+
+gulp.task('copy_jquery', function() {
+	return gulp.src(paths.jquery + '/jquery.min.js')
+		.pipe(gulp.dest(paths.js));
+});
+
+// Copy stuff
+gulp.task('copy', ['copy_bootstrapjs', 'copy_jquery']);
+
+// Watch
+gulp.task('watch', function() {
+     gulp.watch(paths.sass + '/**/*.scss', ['sass']); 
+	gulp.watch(paths.templates + '/**/*.html', ['html']); 
+});
+
+// Default
+  gulp.task('default', ['html', 'sass']);
